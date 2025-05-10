2815749c3649171eefb06faf2fde59308f6d6988
Farzin Ebrahimi <farzinebrahimi.info@gmail.com>
2025-05-10 22:21:40 +0330

    ADD: generate token interface and init token service

--- Shop-API/Interfaces/ITokenService.cs
+++ Shop-API/Interfaces/ITokenService.cs
@@ -1,6 +1,8 @@
+using Shop_API.Entities.Users;
+
 namespace Shop_API.Interfaces;
 
 public interface ITokenService
 {
-    
+    string GenerateToken(AppUser user);
 }
---
+++ Shop-API/Services/TokenService.cs
@@ -0,0 +1,6 @@
+namespace Shop_API.Services;
+
+public class TokenService
+{
+    
+}
