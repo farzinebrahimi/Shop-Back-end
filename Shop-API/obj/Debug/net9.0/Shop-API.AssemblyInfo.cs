fe0fdaa84ae5b283b89ee09e3b1661931c7a65fb
Farzin Ebrahimi <farzinebrahimi.info@gmail.com>
2025-05-11 23:00:30 +0330

    ADD:Error Controller

--- Shop-API/obj/Debug/net9.0/Shop-API.AssemblyInfo.cs
+++ Shop-API/obj/Debug/net9.0/Shop-API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Shop-API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+10f2bfe8f00b6d322a060aa1cfe3e6ba3e1397a5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ed71d3f0be17cc3d06422528e682ce8d1e061f49")]
 [assembly: System.Reflection.AssemblyProductAttribute("Shop-API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Shop-API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
